1c19db18e25e75590858b52ad5c14dec0c9a957e
sohee52 <wneh2023@naver.com>
2026-01-05 16:13:23 +0900

    0010

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,10 +1,16 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.post.post.document.Post;
+import com.back.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+
     public long count() {
-        return 0;
+        return postRepository.count();
     }
 }
